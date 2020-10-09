fea75f7b2aa10fa61c112d11b20ee1d7a8499f01
tathyab <tathy.ab@gmail.com>
2020-10-08 21:13:14 -0300

    Comitando o Primeiro exemplo do professor.

--- js/primeiro.js
+++ js/primeiro.js
@@ -2,28 +2,47 @@ var scene;
 var camara;
 var renderer;
 
+var velocidadeCubo = 0.5;
+var cube;
 
 var criaCubo = function(){
-    var geometria = new THREE().BoxGeometry(10,10,10);
-    var material = new THREE().MeshBasicMaterial({color: "blue"});
-    cube = new THREE.Mesch(geometria, material);
+    var geometria = new THREE.BoxGeometry(10,10,10);
+    var material = new THREE.MeshBasicMaterial({color: "blue"});
+	
+    cube = new THREE.Mesh(geometria, material);
 
     scene.add(cube);
 }
 
-var int = function () {
-    scene = new THREE().Scene();
-    camera = new THREE().PerpesctiveCamera(40, innerWidth/window.innerHeight, 1, 1000);
+var render = function (){
+    requestAnimationFrame(render);
 
-    renderer = new THREE().WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    camera.position.z = 50;
-
-    criaCubo();
-
+    animaCubo();
     renderer.render(scene,camera);
 }
 
-window.onload= this.init;
+var animaCubo = function(){
+    
+    if (this.cube.position.x >= 75 || this.cube.position.x <= -75){
+        velocidadeCubo = velocidadeCubo * -1;   
+    }
+    this.cube.position.x += velocidadeCubo;
+    console.log("Posicao Cubi" + this.cube.position.x); 
+}
+
+var init = function (){
+    scene = new THREE.Scene;
+    camera = new THREE.PerspectiveCamera(40, innerWidth/window.innerHeight, 1, 1000);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    camera.position.z = 100;
+	
+    criaCubo();
+    
+    render();
+}
+
+window.onload = this.init;
